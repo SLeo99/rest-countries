f04bcf6f3819da78d3981a65ccc2266741790385
SLeo99 <leonelsg1999@gmail.com>
2023-05-05 18:02:49 -0300

    sort components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import FilterSearch from "./components/FilterSearch";
-import CountryDetail from "./components/CountryDetail";
 import Header from "./components/Header";
+import CountryDetail from "./components/CountryDetail";
+import FilterSearch from "./components/FilterSearch";
 import CountriesList from "./components/CountriesList";
 
 
