7870cda4c8fd5f0284b728b49e5e7b507fc8fd04
SLeo99 <leonelsg1999@gmail.com>
2023-05-08 16:01:58 -0300

    dynamic links

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,42 @@ import Header from "./components/Header";
 import CountryDetail from "./components/CountryDetail";
 import FilterSearch from "./components/FilterSearch";
 import CountriesList from "./components/CountriesList";
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { useState } from "react";
+import useFetch from "./hooks/useFetch";
+
 
 
 function App() {
+
+  const [country,setCountry] = useState(null)
+
+  function handleCountry(e) {
+    setCountry(e)
+  }
+
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      {/* <FilterSearch /> */}
-      {/* <CountriesList /> */}
-      <CountryDetail />
-    </>
+      <Routes>
+        <Route path='/' element=
+        { 
+          <>
+            <FilterSearch />
+            <CountriesList 
+            countrylink={handleCountry}
+            />
+          </>
+        }
+        />
+        <Route path={`/:${country}`} element=
+          {
+          <CountryDetail 
+          countrydetails={country}/>
+          } 
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,7 +1,8 @@
 import CountriesCard from "./CountriesCard";
 import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
 
-export default function CountriesList() {
+export default function CountriesList({countrylink}) {
 
   const ALL_COUNTRIES = 'https://restcountries.com/v3.1/all'
   const data = useFetch(ALL_COUNTRIES)
@@ -9,14 +10,19 @@ export default function CountriesList() {
   return (
     <main className="w-10/12 sm:w-[70.5%] mx-auto grid gap-[80px] xl:gap-[76px] xl:container lg:grid-cols-fluid xl:w-[88.75%]  2xl:max-w-[91.5%]">
       {data.map((item, i) => (
-        <CountriesCard 
+        <Link 
         key={i} 
-        img={item.flags.svg} 
-        country={item.name.common} 
-        population={item.population} 
-        region={item.region} 
-        capital={item.capital}>
-        </CountriesCard>
+        to={item.name.common}
+        onClick={() => countrylink(item.name.common)}
+        >
+          <CountriesCard 
+          img={item.flags.svg} 
+          country={item.name.common} 
+          population={item.population} 
+          region={item.region} 
+          capital={item.capital}
+          />
+        </Link>
       ))}
     </main>
   )
